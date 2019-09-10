2148cfdb55993e35ba057fb34c10eee7351d3a9a
aman-maharshi <amanm4harshi@gmail.com>
2019-09-10 23:39:05 +0530

    added a bit of ES6 code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,24 @@
-function test(testName) {
-    console.log('This is a ' + testName + ' Test')
+class Person {
+    constructor(fullName, favBand) {
+        this.name = fullName;
+        this.band = favBand;
+    }
+
+    // methods
+    greet() {
+        console.log('Yo, I\'m ' + this.name + ' and I like ' + this.band );
+    }
 }
-test('dummy');
+
+class Entertain extends Person {
+    watch() {
+        console.log(this.name + ' is going out to the movies tonight!')
+    }
+}
+
+let john = new Person('John', 'Nirvana');
+john.greet();
+
+let jane = new Entertain('Jane', 'Metallica');
+jane.greet();
+jane.watch();
